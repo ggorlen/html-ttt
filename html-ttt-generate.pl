a7ba3646f213e16942aa117abad931e709ece68c
ggorlen <gsgorlen@gmail.com>
2026-02-03 14:26:23 -0800

    Add initial scale meta tag

--- html-ttt-generate.pl
+++ html-ttt-generate.pl
@@ -142,8 +142,9 @@ body {
     my $header = qq{<!DOCTYPE html>
 <html lang="en">
 <head>
-<title>tic tac toe</title>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+<title>tic tac toe</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body><div id="ttt">};
